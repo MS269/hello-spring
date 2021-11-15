a514e251e5b1dd3a3b00807d9206633f8e83d416
MS269 <ehddnr9797@gmail.com>
2021-11-15 23:45:41 +0900

    Commit AOP

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,10 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.*;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -24,12 +21,17 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
-//    @Bean
-//    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-//        return new JpaMemberRepository(em);
-//    }
+    // @Bean
+    // public TimeTraceAop timeTraceAop() {
+    // return new TimeTraceAop();
+    // }
+
+    // @Bean
+    // public MemberRepository memberRepository() {
+    // return new MemoryMemberRepository();
+    // return new JdbcMemberRepository(dataSource);
+    // return new JdbcTemplateMemberRepository(dataSource);
+    // return new JpaMemberRepository(em);
+    // }
 
 }
